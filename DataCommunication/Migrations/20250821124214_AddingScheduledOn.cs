0ef5c80daf8c1455474d301bf6d6d80003951bea
Vineet Singh <vineet.u@cisinlabs.com>
2025-08-21 18:13:01 +0530

    Added ScheduledOn

---
+++ DataCommunication/Migrations/20250821124214_AddingScheduledOn.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lbbak_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingScheduledOn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_EventCongratulators_Users_UserId",
+                table: "EventCongratulators");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "StartDate",
+                table: "Events",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EndDate",
+                table: "Events",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ScheduledOn",
+                table: "Events",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserId",
+                table: "EventCongratulators",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Message",
+                table: "EventCongratulators",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "GiftAmount",
+                table: "EventCongratulators",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "RecipientId",
+                table: "EventCongratulators",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RecipientMobile",
+                table: "EventCongratulators",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RecipientName",
+                table: "EventCongratulators",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EventCongratulators_RecipientId",
+                table: "EventCongratulators",
+                column: "RecipientId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_EventCongratulators_Users_RecipientId",
+                table: "EventCongratulators",
+                column: "RecipientId",
+                principalTable: "Users",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_EventCongratulators_Users_UserId",
+                table: "EventCongratulators",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_EventCongratulators_Users_RecipientId",
+                table: "EventCongratulators");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_EventCongratulators_Users_UserId",
+                table: "EventCongratulators");
+
+            migrationBuilder.DropIndex(
+                name: "IX_EventCongratulators_RecipientId",
+                table: "EventCongratulators");
+
+            migrationBuilder.DropColumn(
+                name: "ScheduledOn",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "RecipientId",
+                table: "EventCongratulators");
+
+            migrationBuilder.DropColumn(
+                name: "RecipientMobile",
+                table: "EventCongratulators");
+
+            migrationBuilder.DropColumn(
+                name: "RecipientName",
+                table: "EventCongratulators");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "StartDate",
+                table: "Events",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EndDate",
+                table: "Events",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserId",
+                table: "EventCongratulators",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Message",
+                table: "EventCongratulators",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "GiftAmount",
+                table: "EventCongratulators",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_EventCongratulators_Users_UserId",
+                table: "EventCongratulators",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
