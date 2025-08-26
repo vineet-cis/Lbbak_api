18bbe64572c345ffe533b20a17bc8dbc77337379
Vineet Singh <vineet.u@cisinlabs.com>
2025-08-26 14:38:00 +0530

    adding countryId

---
+++ DataCommunication/Migrations/20250826090607_UpdatingFields.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lbbak_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatingFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CountryId",
+                table: "City",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Country",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Country", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_City_CountryId",
+                table: "City",
+                column: "CountryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_City_Country_CountryId",
+                table: "City",
+                column: "CountryId",
+                principalTable: "Country",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_City_Country_CountryId",
+                table: "City");
+
+            migrationBuilder.DropTable(
+                name: "Country");
+
+            migrationBuilder.DropIndex(
+                name: "IX_City_CountryId",
+                table: "City");
+
+            migrationBuilder.DropColumn(
+                name: "CountryId",
+                table: "City");
+        }
+    }
+}
